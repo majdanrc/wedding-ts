fa061d1c5e2ae1ed27e7614ca14de8d318edc4e4
majdanrc <rmarkiew@gmail.com>
2020-06-21 14:31:42 +0200

    trigger github action

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export type ServiceYear = 2020 | 2021 | 2022;
 export type ServiceType = "Photography" | "VideoRecording" | "BlurayPackage" | "TwoDayEvent" | "WeddingSession";
 
+type ServiceRelations = Record<ServiceType, ServiceType[]>;
+
 export const updateSelectedServices = (
     previouslySelectedServices: ServiceType[],
     action: { type: "Select" | "Deselect"; service: ServiceType }
